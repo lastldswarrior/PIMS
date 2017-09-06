4f94fa209f9e5794420ece231398e91c53b65419
Jared <Jared@SchoolNinja>
2017-09-06 09:58:05 -0500

    First Commit, Webpage talking to Database

---
+++ src/java/pims/DBConnect.java
@@ -0,0 +1,60 @@
+package pims;
+
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Jared
+ */
+public class DBConnect {
+
+    public DBConnect() {
+
+    }
+    
+    public String returnThis(String string){
+        return string;
+    }
+
+    public String validate(String name, String pass) {
+        try {
+            PreparedStatement pst = null;
+            ResultSet rs = null;
+            
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/localDB", "PatientDB", "ninjas");
+            
+            if (!conn.isClosed()) {
+                System.out.println("Connected to: " + conn.getSchema());
+            }
+            String statement = "select * from PATIENTDB.USERS where USERS.USER_NAME='"+name+"' and USERS.USER_PASSWORD='"+pass+"'";
+            
+            pst = conn.prepareStatement(statement);
+            rs = pst.executeQuery();
+            String last = null;
+            while (rs.next()) {
+                last = rs.getString("LEVEL");
+            }
+            return last;
+            
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null,ex);
+            return null;
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        
+    }
+}
+
+
---
+++ web/index.jsp
@@ -0,0 +1,34 @@
+<%-- 
+    Document   : index
+    Created on : Sep 6, 2017, 7:07:51 AM
+    Author     : Jared
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+   <head>
+        <title>Login Application</title>
+    </head>
+    <body>
+        <form action="ControllerServlet" method="post">
+            <fieldset style="width: 300px">
+                <legend> Login to App </legend>
+                <table>
+                    <tr>
+                        <td>User ID</td>
+                        <td><input type="text" name="username" required="required" /></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password" name="userpass" required="required" /></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" value="Login" /></td>
+                    </tr>
+                </table>
+            </fieldset>
+             ${pass}<%-- This one works --%>
+        </form>
+    </body>
+</html>
